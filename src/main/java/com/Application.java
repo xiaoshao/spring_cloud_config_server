575baa6786a02c62a0d4c17887c8693346613055
zengwei shao <zwshao@thoughtworks.com>
2016-11-20 11:31:59 +0800

    add eureka server

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,21 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@RestController
+//@RestController
 @EnableConfigServer
+@EnableEurekaServer
 public class Application {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String home(){
-        return "home";
-    }
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String home(){
+//        return "home";
+//    }
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
